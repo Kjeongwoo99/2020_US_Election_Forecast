b6ccee0955121654237d12db859eedf2594b0115
Jiwon Choi <jwon.choi@mail.utoronto.ca>
2024-03-09 15:06:47 -0500

    Update .gitignore and 01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,10 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data 
+# Author: Jeongwoo Kim, Jiwon Choi
+# Date: 8 March 2024
+# Contact: jwon.choi@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: 00-simulation-survey.R, 00-simulation-poststratification.R
 
-
-#### Workspace setup ####
-library(opendatatoronto)
-library(tidyverse)
-# [...UPDATE THIS...]
-
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
-
-
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+# We use two datasets, one is the survey data from Voter Study Group, and the other is ACS data from IPUMS USA.
+# We cannot share these files, but the steps we took to obtain them are documented in the README.md file.
